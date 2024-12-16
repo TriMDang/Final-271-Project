3dbca4500904a0ef9dd0775f6540f01f654e36e8
Dang <dang_t2@red9.cs.denison.edu>
2024-12-16 16:51:02 -0500

    revising graph

--- WeightedGraph.hpp
+++ WeightedGraph.hpp
@@ -1,8 +1,8 @@
 //=========================================
 // Graph.hpp
 // Date: 12/6/2024
-// Author: Tri Dang, Andrew Nguyen, Aisha Barry
-// Description: Declaration of all functions and variables for graph
+// Author: Tri Dang, Omar Perez, Esther Zhang
+// Description: Declaration of all functions and variables for Weighted graph
 //=========================================
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
@@ -21,33 +21,33 @@ using namespace std;
 
 class Graph {
 private:
-    unordered_map<int, vector<int> > adjacencyList; // stores edges
-    //unordered_map<int, tuple<double, double>> vertices; // store vertices, <key, <x, y>>
-    //unordered_map<int, vector<tuple<int, int, string>>> adjacencyList // store edges; <u, <v, weight, name>>
-    vector<int> sorted; // for topological order
-    vector<int> ordered; // final topological order
-    unordered_map<int, tuple<int, int, int>> DFSresults; // for DFS, format: unordered_map< vertex u , tuple< discovery, finish, parent >>
-    long finish; // f in textbook
-    long discovery; // d in textbook
-    long time; // will be kept global here for accurate and consistent finish and discovery time
+    unordered_map<int, tuple<double, double>> vertices; // store vertices, <vertex key, <x, y>>
+    unordered_map<int, vector<tuple<int, double, string>>> adjacencyList // store edges; <u, <v, weight, name>>
+    
+    //unordered_map<int, vector<int> > adjacencyList; // stores edges
+    //vector<int> sorted; // for topological order
+    //vector<int> ordered; // final topological order
+    //unordered_map<int, tuple<int, int, int>> DFSresults; // for DFS, format: unordered_map< vertex u , tuple< discovery, finish, parent >>
+    //long finish; // f in textbook
+    //long discovery; // d in textbook
+    //long time; // will be kept global here for accurate and consistent finish and discovery time
 
 public:
     Graph(); // Default constructor
     ~Graph(); // Destructor
     Graph(const Graph& other);   // Copy constructor
     Graph& operator=(const Graph& other);
-
-
-    void addEdge(int u, int v);
-    void removeEdge(int u, int v);
-    bool edgeIn(int u, int v);
-    void deleteVertex(int u);
-    void addVertex(int u);
-    unordered_map<int, pair<int, int> > breadthFirstSearch(int s);
-    unordered_map<int, tuple<int, int, int> > depthFirstSearch(bool sort=false);
-    void DFSvisit(int u, int parent, set<int> visited, bool sort=false);
-    vector<int> getOrdering();
     static Graph readFromSTDIN();
+    void addEdge(int u, int v, double weight, string name=""); // name="" prevents line with no names
+    void addVertex(int u, double x, double y);
+
+    //bool edgeIn(int u, int v);
+    //void deleteVertex(int u);
+    //void addVertex(int u);
+    //unordered_map<int, pair<int, int> > breadthFirstSearch(int s);
+    //unordered_map<int, tuple<int, int, int> > depthFirstSearch(bool sort=false);
+    //void DFSvisit(int u, int parent, set<int> visited, bool sort=false);
+    //vector<int> getOrdering();
 };
 // Add other methods similarly
 
