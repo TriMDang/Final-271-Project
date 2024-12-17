445f38e96f54f64f3b2961b3ca6e643ff38c3b68
Esther Zhang <zhang_e1@denison.edu>
2024-12-17 00:17:24 -0500

    Update PriorityQueue.hpp
    Rough hpp file may be missing some things

--- PriorityQueue.hpp
+++ PriorityQueue.hpp
@@ -1,1 +1,38 @@
+//=========================================
+// PriorityQueue.hpp
+// Date: 12/16/24
+// Authors: Tri Dang, Omar Perez, Esther Zhang
+// Description: Declaration of all functions and variables for Priotity Queue
+//=========================================
+
+#ifndef PRIORITYQUEUE_HPP
+#define PRIORITYQUEUE_HPP
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class MinPQueue {
+    private:
+        vector<long> heap; //Store the values of the priority queue
+        void heapify(int i);
+        void builHeap();
+        int left(i);
+        int right(i);
+
+    public:
+        MinPQueue(); //Default constructor
+        ~MinPQueue(); //Destructor
+        MinPQueue(const MinPQueue& other); //Copy constructor
+        MinPQueue& operator=(const MinPQueue& other);
+        void insert(value);
+        int getMin();
+        int extractMin();
+        void decreaseKey();
+        void remove(value);
+};
+
+#endif
+
 
